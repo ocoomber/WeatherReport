de904bdf095002f6f5618a9148ab033e6bd5d5f3
ocoomber <ocoomber@gmail.com>
2026-07-14 20:23:43 +0100

    replace corrupt inline svg with icon.svg ref, tighten bump regex

--- bump.ps1
+++ bump.ps1
@@ -1,8 +1,8 @@
 $index = [System.IO.File]::ReadAllText("$PSScriptRoot/index.html")
 
-$index = [regex]::Replace($index, 'b(\d+)', { param($m) "b$(([int]$m.Groups[1].Value + 1).ToString('D3'))" })
+$index = [regex]::Replace($index, 'b(\d{3})', { param($m) "b$(([int]$m.Groups[1].Value + 1).ToString('D3'))" })
 
-$buildNum = [regex]::Match($index, 'b(\d+)').Groups[1].Value
+$buildNum = [regex]::Match($index, 'b(\d{3})').Groups[1].Value
 
 $sw = [System.IO.File]::ReadAllText("$PSScriptRoot/sw.js")
 $sw = [regex]::Replace($sw, "(?<=weather-tool-)\w+", "b$buildNum")
