76bc2696205dc460b7954d85b5eed45718eca442
ocoomber <ocoomber@users.noreply.github.com>
2026-07-14 20:02:23 +0100

    unify build number and sw cache version into single bXXX

--- bump.ps1
+++ bump.ps1
@@ -1,8 +1,11 @@
 $index = [System.IO.File]::ReadAllText("$PSScriptRoot/index.html")
-$sw = [System.IO.File]::ReadAllText("$PSScriptRoot/sw.js")
 
 $index = [regex]::Replace($index, 'b(\d+)', { param($m) "b$(([int]$m.Groups[1].Value + 1).ToString('D3'))" })
-$sw = [regex]::Replace($sw, 'weather-tool-v(\d+)', { param($m) "weather-tool-v$([int]$m.Groups[1].Value + 1)" })
+
+$buildNum = [regex]::Match($index, 'b(\d+)').Groups[1].Value
+
+$sw = [System.IO.File]::ReadAllText("$PSScriptRoot/sw.js")
+$sw = [regex]::Replace($sw, "(?<=weather-tool-)\w+", "b$buildNum")
 
 [System.IO.File]::WriteAllText("$PSScriptRoot/index.html", $index)
 [System.IO.File]::WriteAllText("$PSScriptRoot/sw.js", $sw)
